1b9463a3d6c6238a98d5299380dc0ae00b4b758c
santivzqzz <114695520+santivzqzz@users.noreply.github.com>
2022-10-04 01:13:05 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,9 @@
 
 # Bienvenida
 print("Bienvenido a la consulta especializada en dolores abdominales!"
-input("Para empezar indique si usted es hombre o mujer: ").lower() # <-- lo ponemos en minúsculas por si el usuario lo escribe mal y que no haya problemas en el programa.
+      
+# Preguntamos género del paciente
+x = input("Para empezar indique si usted es hombre o mujer: ").lower() # <-- lo ponemos en minúsculas por si el usuario lo escribe mal y que no haya problemas en el programa.
+
+# Preguntamos ubicación del dolor
 
