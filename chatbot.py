e3fc243e4c53be68222ba7a94ab1e627c5299f22
santivzqzz <114695520+santivzqzz@users.noreply.github.com>
2022-10-04 01:09:47 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,5 @@
-print("Hola mundo")
-print("Trabajar en el tema")
+
+# Bienvenida
+print("Bienvenido a la consulta especializada en dolores abdominales!"
+input("Para empezar indique si usted es hombre o mujer: ").lower() # <-- lo ponemos en minúsculas por si el usuario lo escribe mal y que no haya problemas en el programa.
+
