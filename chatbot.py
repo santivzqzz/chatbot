a839807e127394ab9ca85cf93cb0e3de5352eb97
MiguelSanromanRivero <114821260+MiguelSanromanRivero@users.noreply.github.com>
2022-10-12 20:54:23 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,10 +14,15 @@ sintomas = [["dolor","leve"], ["dolor","moderadamente","intenso"], ["dolor","int
             ["dolor","punzante"],["quemazón"],["dolor","vago"],["dolor","comer"],["hambre"],
             ["distensión"],["náuseas"],["vómitos"],["malestar","estomacal"],["sudoración"],
             ["taquicardia"],["sangre","orina"],["ardor","orina"],["dolor","orina"],
-            ["sensación","imperiosa","orinar"],["sagre","heces"],["dolor","evacuación"],
-            ["evacuación","incompleta"],["diarrea"],["diarrea","sangre"],
-            ["protuberancia","indolora","ingle","escroto"],["fiebre"],["ictericia"],
-            ["estreñimiento"],["pérdida","peso"],["cansancio"],["saciedad"]]
+            [["sensación","ganas","necesidad"],["imperiosa","imperiosas","constanstes"],["orinar","miccionar","mear"]],
+            [["sagre","sangrado"],["heces","excremento","excrementos","oculta"]],
+            [["dolor","dolores"],["evacuación","evacuar","evacuacion","excrección","excretar","excrecion","cagar"]],
+            [["evacuación","evacuar","evacuacion","excrección","excretar","excrecion"],["incompleta","no completa","inacabada","no acabada"]],
+            [["diarrea","descomposición","descomposicion","descompuesto"]],
+            [["diarrea","descomposición","descomposicion","descompuesto"],["sangre","sangrado"]],
+            [["bulto","protuberancia"],"indolora",["ingle","escroto"]],["fiebre"],[["piel amarilla","ictericia"]],
+            ["estreñimiento"],[["pérdida","perder"],"peso"],[["cansancio","cansado","fatiga","fatigado","agotado","agotamiento"]],
+            [["saciado","saciedad"]]
 
 wellRedactedSintomas = ["dolor leve", "dolor moderadamente intenso", "dolor intenso",
             "dolor insoportable","retortijones","dolor cólico",
