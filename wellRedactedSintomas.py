321f00028ca94beb2eaaca9cbeed065cb238d074
MiguelSanromanRivero <114821260+MiguelSanromanRivero@users.noreply.github.com>
2022-11-02 16:22:32 +0100

    Add files via upload

---
+++ wellRedactedSintomas.py
@@ -0,0 +1,9 @@
+wellRedactedSintomas = ["dolor leve", "dolor moderadamente intenso", "dolor intenso",
+            "dolor insoportable","retortijones","dolor cólico",
+            "dolor punzante","quemazón","dolor vago","dolor al comer","sensación de hambre",
+            "distensión","náuseas","vómitos","malestar estomacal","sudoración",
+            "taquicardia","sangre en la orina","ardor al orinar","dolor al orinar",
+            "sensación imperiosa de orinar","sagre en las heces","dolor en la evacuación",
+            "evacuación incompleta","diarrea","sangre en la diarrea",
+            "protuberancia indolora en la ingle y en el escroto","fiebre","ictericia",
+            "estreñimiento","pérdida de peso","cansancio","saciedad"]
